163c1aca7bb2b8d71b6b18fe2b1fa3dbf4d88549
Tyler Bombardier <W0870190@myscc.ca>
2025-10-10 20:10:04 -0400

    Added basic button with functional theme switching

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+let root = document.documentElement;
+let toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", ()=>{
+    let current = root.getAttribute("data-theme");
+    if(current === "light"){
+        root.setAttribute("data-theme","dark");
+    }
+    else{
+        root.setAttribute("data-theme","light");
+    }
+});
 
